3db2712b4b55445eda61227ab8c16c3fd7f276b1
Jacob <139719541+j-isreal@users.noreply.github.com>
2024-09-26 18:41:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-        header("Location: https://www.google.com/");
+// Sample PHP root index.php file to redirect all requests to somewhere else
+// CHANGE the URL below to redirect to main website or error document
+header("Location: https://www.google.com/");
 ?>
 
