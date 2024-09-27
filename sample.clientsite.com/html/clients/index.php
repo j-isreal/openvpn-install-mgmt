dade1da34e7bdb3586fe7d31b712b05358bcea5c
Jacob <139719541+j-isreal@users.noreply.github.com>
2024-09-26 20:34:33 -0500

    Update index.php

--- sample.clientsite.com/html/clients/index.php
+++ sample.clientsite.com/html/clients/index.php
@@ -1,3 +1,5 @@
 <?php
-        header("Location: https://sample.clientsite.com/");
+// sends any user request to this folder to another site
+// you can also use the err404.html file in the root of the website
+        header("Location: https://www.google.com/");
 ?>
