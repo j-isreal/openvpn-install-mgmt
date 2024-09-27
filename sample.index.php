9e7b422a2daf31cf3c1a6643fdc5cd61f0197f6c
ICLLC Dev <jisreal@icllc.cc>
2024-09-26 20:41:30 -0500

    Removed unnecessary sample files

--- sample.index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Sample PHP root index.php file to redirect all requests to somewhere else
-// CHANGE the URL below to redirect to main website or error document
-
-header("Location: https://www.google.com/");
-?>
-
